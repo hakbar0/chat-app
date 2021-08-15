3cb8234408d5117eb3d86b14f27f606c4dfef1f8
hakbar0 <haseeb2000@hotmail.com>
2021-08-15 21:59:30 +0100

    managed user with profile context

--- chat-app/src/components/PublicRoute.js
+++ chat-app/src/components/PublicRoute.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router';
+import { Container, Loader } from 'rsuite';
+import { useProfile } from '../context/profile.context';
 
-function PublicRoute({ children, ...routeProps }) {
-  const profile = false;
+const PublicRoute = ({ children, ...routeProps }) => {
+  if (isLoading && !profile) {
+    return (
+      <Container>
+        <Loader center vertical size="md" content="Loading" speed="slow" />
+      </Container>
+    );
+  }
 
-  if (profile) {
+  if (profile && !isLoading) {
     return <Redirect to="/" />;
   }
 
   return <Route {...routeProps}>{children}</Route>;
-}
+};
 
 export default PublicRoute;
