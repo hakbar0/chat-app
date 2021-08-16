a285519dc54230fb4c479d93cb959a821ef49dee
hakbar0 <haseeb2000@hotmail.com>
2021-08-16 14:53:02 +0100

    created reusable editable input

--- chat-app/src/dashboard/index.js
+++ chat-app/src/dashboard/index.js
@@ -1,20 +1,32 @@
 import React from 'react';
-import { Button, Drawer } from 'rsuite';
+import { Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../context/profile.context';
+import EditableInput from './EditableInput';
 
 function Dashboard({ onSignOut }) {
   const { profile } = useProfile();
+
+  const onSave = async newData => {
+    console.log(newData);
+  };
 
   return (
     <>
       <Drawer.Header>
         <Drawer.Title>Dashboard</Drawer.Title>
       </Drawer.Header>
-
       <Drawer.Body>
         <h3>Hey, {profile.name}</h3>
-      </Drawer.Body>
 
+        <Divider />
+        <EditableInput
+          name="nickname"
+          initialValue={profile.name}
+          onSave={onSave}
+          label={<h6 className="mb-2">Nickname</h6>}
+        />
+      </Drawer.Body>
+      >
       <Drawer.Footer>
         <Button block color="red" onClick={onSignOut}>
           Sign out
