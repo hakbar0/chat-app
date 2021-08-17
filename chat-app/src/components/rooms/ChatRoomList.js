55639e16b5be4c13f7225e5eb5e48609fde6c9fc
hakbar0 <haseeb2000@hotmail.com>
2021-08-17 18:06:17 +0100

    created chat room layout

--- chat-app/src/components/rooms/ChatRoomList.js
+++ chat-app/src/components/rooms/ChatRoomList.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Nav, Loader } from 'rsuite';
-import { useRooms } from '../../context/rooms.context';
-import RoomItem from './RoomItem';
 import { Link, useLocation } from 'react-router-dom';
+import { Nav, Loader } from 'rsuite';
+import RoomItem from './RoomItem';
+import { useRooms } from '../../context/rooms.context';
 
-function ChatRoomList({ aboveElHeight }) {
+const ChatRoomList = ({ aboveElHeight }) => {
   const rooms = useRooms();
   const location = useLocation();
 
@@ -14,27 +14,28 @@ function ChatRoomList({ aboveElHeight }) {
       vertical
       reversed
       className="overflow-y-scroll custom-scroll"
-      style={{ height: `calc(100% - ${aboveElHeight}px)` }}
+      style={{
+        height: `calc(100% - ${aboveElHeight}px)`,
+      }}
       activeKey={location.pathname}
     >
       {!rooms && (
         <Loader center vertical content="Loading" speed="slow" size="md" />
       )}
-
       {rooms &&
         rooms.length > 0 &&
         rooms.map(room => (
           <Nav.Item
             componentClass={Link}
-            to={`/chats/${room.id}`}
+            to={`/chat/${room.id}`}
             key={room.id}
-            eventKey={`/chats/${room.id}`}
+            eventKey={`/chat/${room.id}`}
           >
             <RoomItem room={room} />
           </Nav.Item>
         ))}
     </Nav>
   );
-}
+};
 
 export default ChatRoomList;
